559a70cd1bd3055137445ee94edcf15483b2e68d
qyco48nu <sreenivas.shenoy@ovgu.de>
2025-06-10 23:25:11 +0200

    Initial frontend setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🎬 Movie Sentiment Analysis</h1>
+        <p>Frontend component initialized</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
